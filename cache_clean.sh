c0a16ed66b87c035287b8d55858d909aeee2ecf6
Yevhenii Stelmakh <stelmakh.yevhenii.i@gmail.com>
2022-10-25 12:16:59 +0200

    add two scripts for clean cache and 4 updata/upgrade 1.1

--- cache_clean.sh
+++ cache_clean.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-CACHE_CLEAN="` apt-get autoclean > /dev/null&& \
+CACHE_CLEAN="` apt-get autoclean > /dev/null && \
                apt-get autoremove > /dev/null && \
                apt-get clean > /dev/null  && \
                rm -rf /var/lib/apt/lists/* > /dev/null`"
 
 if  echo "$CACHE_CLEAN"; then
-  echo "clean cache done: `date`." >> /home/hesher/done.log && echo "done"
+  echo "clean cache done: `date`." >> /home/hesher/done.log
 else 
   echo "FAIL"
 fi
